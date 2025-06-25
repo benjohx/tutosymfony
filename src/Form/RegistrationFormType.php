d8a9014d01ee51345f2e4da3bd5de9dfa3c6e1ee
benjohx <johab15@yahoo.fr>
2025-06-26 00:18:52 +0200

    Your commit message

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,10 +6,6 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-<<<<<<< HEAD
-=======
-use Symfony\Component\Form\Extension\Core\Type\TextType;
->>>>>>> c9c5f83777b399740c1b403557c219d159b836b5
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -21,39 +17,29 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', TextType::class, [
-                'label' => 'registrationForm.firstname',
-            ])
-            ->add('lastname', TextType::class, [
-                'label' => 'registrationForm.lastname',
-
-            ])
-            ->add('email', TextType::class, [
-                'label' => 'registrationForm.email',
-            ])
+            ->add('firstname')
+            ->add('lastname')
+            ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
-                'label' => 'registrationForm.agreeTerms',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        // 'message' => 'You should agree to our terms.',
+                        'message' => 'You should agree to our terms.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-               
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
-                 'label' => 'registrationForm.password',
                 'constraints' => [
                     new NotBlank([
-                        // 'message' => 'Please enter a password',
+                    
                     ]),
                     new Length([
                         'min' => 6,
-                        // 'minMessage' => 'Your password should be at least {{ limit }} characters',
+                  
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
